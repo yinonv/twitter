4415cfea5d8fcc4de9cdb7d9d7cf6de7b95c14b9
Yinon Vahab <yinonv@gmail.com>
2019-12-16 23:27:00 +0200

    completed milestone 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,43 +2,58 @@ import React from 'react';
 import './style.css';
 import TweetBox from '../../components/TweetBox';
 import Message from '../../components/Message'
+import { createPost, getPosts } from '../../api'
+
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             tweet: '',
-            tweetsArray: null
+            tweetsArray: null,
+            loading: false
         }
         this.messages = null;
     }
     componentDidMount() {
-        this.setState({ tweetsArray: JSON.parse(localStorage.getItem("tweets")) });
+        this.setState({ loading: true })
+        this.getTweets();
+    }
+    async createNewTweet(obj) {
+        const response = await createPost(obj);
+    }
+    async getTweets() {
+        const response = await getPosts();
+        this.setState({ tweetsArray: response.data.tweets, loading: false })
     }
     handleTweet(msg) {
+        if (msg == '') {
+            return;
+        }
         const { tweetsArray } = this.state;
         const date = new Date;
         const newTweet = {
-            user: "Yinon",
-            time: date.toString(),
-            message: msg
+            userName: "Yinon",
+            content: msg,
+            date: date.toISOString()
         }
         let newArray = [];
-        if(tweetsArray == null){
+        if (tweetsArray == null) {
             newArray = [newTweet];
-        } else{
+        } else {
             newArray = [...tweetsArray, newTweet];
         }
-        newArray.sort((a,b) => new Date(b.time) - new Date (a.time));
+        newArray.sort((a, b) => new Date(b.date) - new Date(a.date));
         this.setState({ tweet: msg, tweetsArray: newArray })
-        localStorage.setItem("tweets", JSON.stringify(newArray));
+        this.createNewTweet(newTweet);
     }
     render() {
-        const { tweetsArray } = this.state;
+        const { tweetsArray, loading } = this.state;
         return (
             <div className="home-body-container">
                 <TweetBox handleTweet={(tweet) => this.handleTweet(tweet)} />
-                {tweetsArray != null && tweetsArray.map((tweet, i) => <Message key={i} user={tweet.user} msg={tweet.message} time={tweet.time} />)}
+                {loading && <img src="./loader.gif" className="loader"></img>}
+                {tweetsArray != null && tweetsArray.map((tweet, i) => <Message key={i} user={tweet.userName} msg={tweet.content} time={tweet.date} />)}
             </div>
         )
     }
